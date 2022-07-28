e5c3c22360473ee2aa81bc11993c168fd68a87eb
Sowmik23 <sowmiksarker.2355.csedu@gmail.com>
2022-07-28 23:46:48 +0600

    [staged cp-algos practice codes]

--- Network_Flow/Software_Allocation.cpp
+++ Network_Flow/Software_Allocation.cpp
@@ -1,104 +1,107 @@
-#include <bits/stdc++.h>
-#include <stdio.h>
-#include <cstdio>
-#include <algorithm>
-#include <cstring>
+#include<iostream>
+#include<string>
+#include<queue>
 
 using namespace std;
 
-char A,B,input[100],input2[20];
-int ans,f,total;
-int res[30][30];
-int visited[30],v1[30];
+int flow[38][38];
+int cap[38][38];
 
-
-
-void augment(int minFlow,int x)
-{
-	if(x==0)
-	{
-		f = minFlow;
-		return;
-	}
-	else if(v1[x]!=-1)
-	{
-		augment(min(minFlow,res[v1[x]][x]),v1[x]);
-		res[v1[x]][x] -=f;
-		res[x][v1[x]] +=f;
-	}
+bool augmenting(){
+    int prev[38];
+    fill(prev,prev+38,-1);
+    
+    queue<int> Q;
+    Q.push(0);
+    prev[0]=-2;
+    
+    int aux;
+    
+    while(!Q.empty()){
+        aux=Q.front();
+        Q.pop();
+        
+        for(int i=0;i<38;i++){
+            if(flow[aux][i]<cap[aux][i] && prev[i]==-1){
+                prev[i]=aux;
+                if(i==37) goto found;
+                Q.push(i);
+            }
+        }
+    }
+    
+    return false;
+    
+    found:
+    
+    int cur=37,next;
+    
+    while(cur!=0){
+        next=prev[cur];
+        flow[next][cur]++;
+        flow[cur][next]--;
+        cur=next;
+    }
+    
+    return true;
 }
 
-void print_letter(int i)
-{
-	for(int j=1;j<27;j++)
-	{
-		if(res[i][j] ==1)
-		{
-			printf("%c",'A' + j-1);
-			return ;
-		}
-	}
-	printf("_");
-}
-
-int main()
-{
-	while(1)
-	{
-		if(gets(input)==0) break;
-		memset(res,0,sizeof(res));
-		total = 0;
-		
-		while(gets(input))
-		{
-			if(strcmp(input,"")==0) break;
-			sscanf(input,"%c%c %s",&A,&B,input2);
-			res[0][A - 'A' + 1] = B - '0';
-			total +=B - '0';
-			for(int i=27;i<37;i++)  //27 to 36 are computers;
-			{
-				res[i][37] = 1;
-			}
-			
-			ans = 0;
-			while(1)
-			{
-				memset(v1,-1,sizeof(visited));
-				memset(visited,0,sizeof(visited));
-				visited[0] = 1;
-				queue<int> mq;
-				mq.push(0);
-				f = 0;
-				while(!mq.empty())
-				{
-					int top = mq.front();
-					if(top==37) break;
-					mq.pop();
-					for(int i=0;i<38;i++)
-					{
-						if(res[top][i]>0 and !visited[i])
-						{
-							visited[i] = 1;
-							mq.push(i);
-							v1[i] = top;
-						}
-					}
-				}
-				augment(INT_MAX,37);
-				if(f==0) break;
-				ans +=f;
-			}
-			if(ans==total)
-			{
-				for(int i=27;i<37;i++)
-				{
-					print_letter(i);
-				}
-			}
-			else printf("!");
-			printf("\n");
-		}	
-	}
-	
-	return 0;
+int main(){
+    string s;
+    int N1=1;
+    int N2=N1+10;
+    int a,b,n,total,f;
+    bool found;
+    
+    while(getline(cin,s)){
+        memset(cap,0,sizeof(cap));
+        total=0;
+        
+        do{
+            a=N2+s[0]-'A';
+            n=s[1]-'0';
+            
+            for(int i=3;;i++){
+                if(s[i]==';') break;
+                
+                b=N1+s[i]-'0';
+                cap[b][a]=1;
+            }
+            
+            cap[a][37]=n;
+            total+=n;
+            
+            getline(cin,s);
+        }while(!s.empty());
+        
+        for(int i=0;i<10;i++) cap[0][N1+i]=1;
+        
+        memset(flow,0,sizeof(flow));
+        f=0;
+        
+        while(augmenting()) f++;
+        
+        if(f!=total){
+            cout<<'!'<<endl;
+            continue;
+        }
+        
+        for(int i=0;i<10;i++){
+            found=false;
+            
+            for(int j=0;j<26;j++){
+                if(flow[N1+i][N2+j]==1){
+                    found=true;
+                    cout<<(char)('A'+j);
+                    break;
+                }
+            }
+            
+            if(!found) cout<<'_';
+        }
+        
+        cout<<endl;
+    }
+    
+    return 0;
 }
--- Vjudge_Contest/How Many Games.cpp
+++ Vjudge_Contest/How Many Games.cpp
@@ -13,26 +13,36 @@ int main()
 	cin>>t;
 	while(t--)
 	{
-		double x;
-		cin>>x;
-		double y=x;
-		
-		long long int d=1,cnt=0;
-		while(y!=((long long int)y))
+		string str;
+		cin>>str;
+		int len=str.length();
+		int cnt=0,flag=0;
+		for(int i=len-1;i>=0;i--)
 		{
+			if(str[i]=='.' ) 
+			{
+				flag=1;
+				break;
+			}
 			cnt++;
-			d*=10;
-			y=y*10;
 		}
-		//cout<<d<<" "<<cnt<<endl;
-		long long int p=d*x;
+		if(flag==0) cnt=0;
+		//cout<<cnt<<endl;
+		int num=0;
+		for(int i=0;i<len;i++)
+		{
+			if(str[i]!='.') 
+			{
+				num=num*10 + (str[i]-'0');
+			}
+		}
+		//cout<<num<<endl;
+		int l=pow(10,cnt);
+		int k=gcd(num,l);
+		cout<<l/k<<endl;
 		
-		long long int k = gcd(p,d);
-		
-		//cout<<k<<endl;
-		cout<<d/k<<endl;
-	
 	}	
 	
 	return 0;
 }
+
